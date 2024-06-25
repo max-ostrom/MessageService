f5004da81fb10737165b0a2c3e9db1fb4448e89d
max.ostrom <@none>
2024-06-25 15:39:34 +0200

    Initial commit

---
+++ src/MessageService.cpp
@@ -0,0 +1,11 @@
+﻿#include "MessageService.h"
+#include "protobufModels/message.pb.h"
+using namespace std;
+
+int main()
+{
+	Test m;
+	m.add_id(1);
+	cout << "Hello CMake." << endl;
+	return 0;
+}
